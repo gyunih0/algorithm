f71b00e3149c73cd3ba8dbb702e3ad339cbceb6f
Hogyun Kim <ggyuniH0@gmail.com>
2022-05-24 19:30:17 +0900

    Algorithm

--- 개념 구현/binary_tree.py
+++ 개념 구현/binary_tree.py
@@ -1,24 +1,26 @@
 from collections import deque
 
+
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 def make_tree_by(lst, idx):
     parent = None
+
     if idx < len(lst):
         value = lst[idx]
-        if value == None:
+        if value is None:
             return
 
         parent = TreeNode(value)
         parent.left = make_tree_by(lst, 2 * idx + 1)
-        parent.left = make_tree_by(lst, 2 * idx + 2)
+        parent.right = make_tree_by(lst, 2 * idx + 2)
 
     return parent
-
 
 
 def test_max_depth(lst):
@@ -31,7 +33,7 @@ def test_max_depth(lst):
 
     while q:
         depth += 1
-        for _ in range(len(q)):
+        for _ in range(len(q)):  # 같은 레벨에 있는 모든 노드
             cur = q.popleft()
             if cur.left:
                 q.append(cur.left)
--- 개념 구현/doubly_linked_list.py
+++ 개념 구현/doubly_linked_list.py
@@ -12,6 +12,7 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = 0
 
     def is_empty(self):
         if self.head:
@@ -28,18 +29,56 @@ class DoublyLinkedList:
             self.tail = new_node
 
         else:
-            self.head.prev = new_node
-            new_node.next = self.head
+            self.head.prev = new_node  # new_node <- head
+            new_node.next = self.head  # new_node -> head
 
             self.tail = self.head
             self.head = new_node
 
+            # tail <-> head
+            self.head.prev = self.tail
+            self.tail.next = self.head
+
+            self.size += 1
+
     def insert_end(self, item):
         new_node = Node(item)
-        self.tail.next = new_node
-        new_node.prev = self.tail.next
+
+        new_node.next = self.head  # new_node -> head
+        self.head.prev = new_node  # new_node <- head
+
+        self.tail.next = new_node  # tail -> new_node
+        new_node.prev = self.tail.next  # tail <- new_node
 
         self.tail = new_node
+
+        self.size += 1
+
+    def insert_after(self, index, item):
+
+        new_node = Node(item)
+
+        if index >= self.size:
+            print('Out of Index')
+            return False
+
+        # if index >= self.size / 2:  # index 가 tail 쪽이면
+        #     node = self.tail
+        #     for _ in range(self.size - index):
+        #         node = node.prev
+        #
+        #     new_node.next = node
+        #     new_node.prev = node.prev
+        #     node.prev.next = new_node
+        #     node.prev = new_node
+
+            # self.size += 1
+
+
+
+
+
+
 
     def print_all(self):
         if self.is_empty():
@@ -48,8 +87,8 @@ class DoublyLinkedList:
         result = '[head] '
         node = self.head
 
-        while node:
-            if not node.next:
+        for i in range(self.size + 1):
+            if i == self.size:
                 result += '[' + str(node.item) + '] [tail]'
 
             else:
@@ -63,6 +102,8 @@ test = DoublyLinkedList()
 
 test.insert_start(1)
 test.insert_end(2)
+test.insert_end(3)
+test.insert_end(4)
 
-
+test.insert_after(2, 0)
 test.print_all()
